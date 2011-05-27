35a620d7aa41f993197e4495ff5c95931c4ab1e3
Anthony Broussard <anthony@chaione.com>
2011-05-27 14:05:56 -0500

    Added UILabel CHAdditions for resize to fit text

---
+++ UILabel+CHAdditions.m
@@ -0,0 +1,31 @@
+//
+//  UILabel+CHAdditions.m
+//  ChaiOneUI
+//
+//  Created by Anthony Broussard on 5/27/11.
+//  Copyright 2011 ChaiONE. All rights reserved.
+//
+
+#import "UILabel+CHAdditions.h"
+
+
+@implementation UILabel (CHAdditions)
+
+-(void)resizeToFitText:(NSString *)text maxSize:(CGSize)maxSize {
+	self.numberOfLines = 0;
+	self.lineBreakMode = UILineBreakModeWordWrap;
+    
+	CGSize expectedLabelSize = [text sizeWithFont:self.font
+                                constrainedToSize:maxSize 
+                                    lineBreakMode:self.lineBreakMode]; 
+	
+	//adjust the label the the new height.
+	CGRect newFrame = self.frame;
+	newFrame.size.height = expectedLabelSize.height;
+	self.frame = newFrame;
+	
+	self.text = text;	
+}
+
+
+@end
