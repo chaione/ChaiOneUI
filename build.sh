0ecb2588f3fdfcd64dad86b0dbce1d3484d1b540
Ben Scheirman <ben@scheirman.com>
2011-12-22 11:36:38 -0600

    Fix build for multiple platforms/architectures.  Updated build script to run simulator and device builds.

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 set -e
 
+echo "Updating submodules..."
 git submodule update --init --recursive
-xcodebuild -workspace ChaiOneUI.xcworkspace -scheme ChaiOneUI -configuration Debug -sdk iphonesimulator5.0 clean build
 
+echo "Building for iPhone Simulator 5.0..."
+xcodebuild -workspace ChaiOneUI.xcworkspace -scheme ChaiOneUI -configuration Release -sdk iphonesimulator5.0 clean build
+
+echo "Building for iPhone Device 5.0..."
+xcodebuild -workspace ChaiOneUI.xcworkspace -scheme ChaiOneUI -configuration Release -sdk iphoneos5.0 clean build
