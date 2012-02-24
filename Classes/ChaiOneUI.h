1f1c0795a1077d25d2a055b84115c92ec4c75b99
Ben Scheirman <ben@scheirman.com>
2012-02-23 21:17:00 -0600

    Added a single header file for easier importing

---
+++ Classes/ChaiOneUI.h
@@ -0,0 +1,37 @@
+//
+//  ChaiOneUI.h
+//  ChaiOneUI
+//
+//  Created by Ben Scheirman on 2/23/12.
+//  Copyright (c) 2012 ChaiONE. All rights reserved.
+//
+
+#ifndef ChaiOneUI_ChaiOneUI_h
+#define ChaiOneUI_ChaiOneUI_h
+
+// Macros
+#import "CHUIMacros.h"
+
+// Classes
+#import "CHAlertView.h"
+#import "CHAutocompleteDelegate.h"
+#import "CHAutocompleteSearchViewController.h"
+#import "CHBarButtonItem.h"
+#import "CHButtonStrip.h"
+#import "CHHTMLContentViewController.h"
+#import "CHLoginAlertViewController.h"
+#import "CHModalPickerView.h"
+#import "CHPickerViewSensitiveScrollView.h"
+#import "CHSegmentedControl.h"
+#import "CHSplashScreenViewController.h"
+#import "CHStretchableImageSegmentedControl.h"
+
+// Categories
+#import "UITableView+CHAdditions.h"
+#import "UIAlertView+CHSimpleAlerts.h"
+#import "UILabel+CHAdditions.h"
+#import "UIView+CHRectManipulation.h"
+#import "UIButton+CHFlipButton.h"
+#import "UIDevice+CHUniqueIdentifier.h"
+
+#endif
